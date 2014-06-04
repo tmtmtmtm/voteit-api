d6c5313339df34b974287fcb2cbcaea7e31a8a22
Tony Bowden <tony@tmtm.com>
2014-06-04 11:44:02 +0300

    Add bulk loaders
    bulk_load_{parties,people,motions}

--- voteit/loader.py
+++ voteit/loader.py
@@ -10,12 +10,22 @@ def reload_people(data):
         person['@type'] = 'Person'
         persons.update({'id': person.get('id')}, person, upsert=True)
 
+def bulk_load_people(data):
+    for doc in data:
+        doc['_id'] = doc.get('id')
+    persons.insert(data)
+
 
 def reload_parties(data):
     for party in data:
         print "PTY Loading: %s" % party.get('name')
         party['@type'] = 'Party'
         parties.update({'id': party.get('id')}, party, upsert=True)
+
+def bulk_load_parties(data):
+    for doc in data:
+        doc['_id'] = doc.get('id')
+    parties.insert(data)
 
 
 def reload_motions(data):
@@ -30,13 +40,44 @@ def reload_motions(data):
             vote_event['id'] = vote_event_id
             vote_event['motion_id'] = motion_id
             vote_events.update({'id': vote_event_id}, vote_event, upsert=True)
-            
+
             for count in vote_event.get('counts'):
                 count['vote_event_id'] = vote_event_id
                 vote_counts.update({'vote_event_id': vote_event_id, 'option': count.get('option')}, count, upsert=True)
 
             for vote in vote_event.get('votes'):
                 reload_vote(vote, vote_event, motion, data)
+
+def bulk_load_motions(data):
+    for motion in data:
+        motion['_id'] = motion.get('id')
+        print "Inserting motion: %s" % motion['_id']
+
+        for vote_event in motion.get('vote_events'):
+            # Short cut for 1:1 motion:vote_event cases
+            vote_event_id = vote_event.get('id') or "VE-%s" % motion['_id']
+            vote_event['_id'] = vote_event_id
+            vote_event['id'] = vote_event_id
+            vote_event['motion_id'] = motion['_id']
+
+            # Do we need these at all?
+            for vote_count in vote_event.get('counts'):
+                vote_count['_id'] = vote_count.get('id') or "VC-%s-%s" % (vote_event_id, vote_count.get('option'))
+                vote_count['id'] = vote_count['_id']
+                vote_count['vote_event_id'] = vote_event_id
+            vote_counts.insert(vote_event.get('counts'))
+
+            for vote in vote_event.get('votes'):
+                vote['_id'] = vote.get('id') or "V-%s-%s" % (vote_event_id, vote.get('voter_id'))
+                vote['id'] = vote['_id']
+                vote['weight'] = vote.get('weight') or 1
+                vote['vote_event_id'] = vote_event_id
+                vote['motion_id'] = motion['_id']
+            votes.insert(vote_event.get('votes'))
+
+        vote_events.insert(motion.get('vote_events'))
+
+    motions.insert(data)
 
 
 def reload_vote(vote, vote_event, motion, data):
--- voteit/manage.py
+++ voteit/manage.py
@@ -4,8 +4,9 @@ from flask.ext.script import Manager
 
 from voteit.core import db, issues
 from voteit.web import app
-from voteit.loader import reload_motions
-from voteit.loader import reload_parties, reload_people
+from voteit.loader import reload_motions, bulk_load_motions
+from voteit.loader import reload_parties, bulk_load_parties
+from voteit.loader import reload_people, bulk_load_people
 
 
 manager = Manager(app)
@@ -25,16 +26,36 @@ def reloadpeople(file_name):
         reload_people(data)
 
 @manager.command
+def bulkloadpeople(file_name):
+    with open(file_name, 'rb') as fh:
+        data = json.load(fh)
+        bulk_load_people(data)
+
+
+@manager.command
 def reloadparties(file_name):
     with open(file_name, 'rb') as fh:
         data = json.load(fh)
         reload_parties(data)
 
 @manager.command
+def bulkloadparties(file_name):
+    with open(file_name, 'rb') as fh:
+        data = json.load(fh)
+        bulk_load_parties(data)
+
+
+@manager.command
 def reloadmotions(file_name):
     with open(file_name, 'rb') as fh:
         data = json.load(fh)
         reload_motions(data)
+
+@manager.command
+def bulkloadmotions(file_name):
+    with open(file_name, 'rb') as fh:
+        data = json.load(fh)
+        bulk_load_motions(data)
 
 @manager.command
 def reset():
